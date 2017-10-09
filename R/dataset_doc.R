5f92a56144f503270e3679b04b2478e80790cee1
LauraPS1 <lpsantamaria@iib.upm.es>
2017-10-09 12:02:56 +0200

    Initial commit

---
+++ R/dataset_doc.R
@@ -0,0 +1,14 @@
+#' GeneSet objects from ChIP-Seq experiments
+#'
+#' List of 1059 GeneSets containing Entrez Gene IDs from genes assigned to
+#' 1059 ChIP-Seq experiments.
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name gene_set_list
+#' @usage data("gene_set_list")
+#' @format a list of 1059 GeneSet objects.
+"gene_set_list"
+
+#' @import GSEABase
